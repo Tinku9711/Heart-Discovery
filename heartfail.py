690b0c059623baccaf2fdda98b386fd9ddd7f4b2
AnonymousOfficialUser <shashanksi2005@gmail.com>
2020-09-05 21:18:48 +0530

    Update heartfail.py

--- heartfail.py
+++ heartfail.py
@@ -1,3 +1,5 @@
+#imports
+
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
@@ -6,17 +8,25 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 import pickle
-data = pd.read_csv("heartfailure.csv")
 
+#read csv file containing dataset
+data = pd.read_csv(r"C:\Users\shashank\HeartFailData\heartfinal.csv")
+
+#fill empty cells
+data = data.fillna(0)
+
+#exclude prediction result column
 y = data["DEATH_EVENT"]
-
 X = data.drop("DEATH_EVENT", axis=1)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.25)
+#split the train and test data 
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.2)
 
-classifier = RandomForestClassifier(n_estimators = 100, criterion = 'entropy', random_state = 0)
-#classifier = LogisticRegression(random_state=0, max_iter=1000000)
-#classifier = KNeighborsClassifier(n_neighbors=100)
+
+#using RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 50, criterion = 'entropy', random_state = 0)
+#classifier = LogisticRegression(random_state=0, max_iter=1000)
+#classifier = KNeighborsClassifier(n_neighbors=3)
 
 classifier.fit(X_train, y_train)
 
@@ -24,6 +34,9 @@ pred = classifier.predict(X_test)
 
 acc = accuracy_score(pred, y_test)
 
-print(acc * 100)
-pickle.dump(classifier, open('modelfinal.pickle', 'wb'))        
+#get accuracy if required
+#print(acc * 100)
 
+
+#store trained model
+pickle.dump(classifier, open('modelfinal.pickle', 'wb'))
